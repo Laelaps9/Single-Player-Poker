26c279b88bd6efe8e7d8ce55d4eceb575fd07575
Laelaps9 <laelaps9@tutanota.com>
2022-01-13 01:55:22 -0600

    Deal cards randomly Cards are dealt randomly. Each card is its own struct with a suit and a value.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
+use rand::seq::SliceRandom;
 use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Card {
+    pub suit: String,
+    pub value: char,
+}
+
+impl Card {
+    pub fn new(s: String, val: char) -> Card {
+        let suit = s.clone();
+        let value = val;
+
+        Card {
+            suit,
+            value,
+        }
+    }
+}
 
 pub fn generate_deck() -> HashMap<String, Vec<char>> {
     let mut deck = HashMap::new();
-    let mut cards = vec!['A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K'];
+    let cards = vec!['A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K'];
 
     deck.insert("Clubs".to_string(), cards.clone());
     deck.insert("Hearts".to_string(), cards.clone());
@@ -10,4 +29,17 @@ pub fn generate_deck() -> HashMap<String, Vec<char>> {
     deck.insert("Diamonds".to_string(), cards.clone());
 
     return deck;
+}
+
+pub fn deal(mut deck: HashMap<String, Vec<char>>) -> Vec<Card> {
+    let suits = ["Clubs", "Hearts", "Spades", "Diamonds"];
+    let mut cards: Vec<Card> = vec![];
+
+    for _i in 0..5 {
+        let suit = suits.choose(&mut rand::thread_rng()).unwrap().to_string();
+        let value = deck.get_mut(&suit).unwrap().choose(&mut rand::thread_rng()).unwrap().clone();
+        cards.push(Card::new(suit, value));
+    }
+
+    return cards;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use single_player_poker as poker;
 
 fn main() {
-    let mut _deck = poker::generate_deck();
+    let mut deck = poker::generate_deck();
+
+    let cards = poker::deal(deck);
+
+    for card in cards {
+        println!("{:?}", card);
+    }
 }
