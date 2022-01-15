cc8afbb9f8889a366c390444b8e9b05e087bc6b7
Laelaps9 <laelaps9@tutanota.com>
2022-01-15 14:19:40 -0600

    Add functionality to remove up to 3 cards Program waits for user input to know which cards to remove to a maximum of 3. Cards are removed from the hand list.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-use std::error::Error;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use std::error::Error;
 use std::fmt;
+use std::io;
 
 #[derive(Debug, PartialEq)]
 pub struct Card {
@@ -45,6 +46,21 @@ impl fmt::Display for Card {
     }
 }
 
+pub fn change_cards(deck: &mut Vec<u8>,
+    cards: &mut Vec<Card>,
+    to_change: Vec<char>) -> Result<(), String> {
+    let mut discarded: Vec<Card> = vec![];
+
+    // Cards are removed now
+
+    for i in to_change {
+        discarded.push(cards.remove((i.to_digit(10).unwrap() - 1).try_into().unwrap()))
+    }
+
+    println!("{:?}", cards);
+
+    return Ok(());
+}
 
 pub fn deal(deck: &mut Vec<u8>) -> Vec<Card> {
     let mut cards: Vec<Card> = vec![];
@@ -65,24 +81,58 @@ pub fn generate_deck() -> Vec<u8> {
     return (1..53).collect::<Vec<u8>>();
 }
 
-
 pub fn run() -> Result<(), Box<dyn Error>> {
     let mut deck = generate_deck();
-    let cards = deal(&mut deck);
+    let mut cards = deal(&mut deck);
+    let mut changed: Vec<Card> = vec![];
 
-    println!("Your cards:");
+    println!("\nYour cards:");
     for (i, card) in cards.iter().enumerate() {
-        println!("{}) {}", i, card);
+        println!("{}) {}", i + 1, card);
     }
+
+    println!("\nType the listed number of the cards you want \
+                to change (e.g. 1 3 4).\
+                \nYou can change up to 3 cards.
+                ");
+    println!("Leave empty and press enter to change none.");
+
+    let mut chars: Vec<char> = vec![];
+    
+    loop {
+        let mut to_change = String::new();
+        io::stdin()
+        .read_line(&mut to_change)
+        .expect("Failed to read input");
+
+        chars = to_change.chars().collect();
+        chars.retain(|c| c.is_numeric());
+
+        if chars.len() <= 3 {
+            break;
+        }
+
+        println!("You can change a maximum of 3 cards");
+        println!("{}", to_change);
+        chars.clear();
+        println!("{:?}", chars);
+
+    }
+
+
+    println!("Cards to change {:?}", chars);
+
+    change_cards(&mut deck, &mut cards, chars);
 
     Ok(())
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // Tests to check if the correct suit is given to the 
+    // Tests to check if the correct suit is given to the
     // first and last card of each
     #[test]
     fn aces() {
@@ -93,7 +143,7 @@ mod tests {
                 value: 1,
             }
         );
-        
+
         assert_eq!(
             Card::new(13),
             Card {
@@ -112,7 +162,7 @@ mod tests {
                 value: 14,
             }
         );
-        
+
         assert_eq!(
             Card::new(26),
             Card {
@@ -131,7 +181,7 @@ mod tests {
                 value: 27,
             }
         );
-        
+
         assert_eq!(
             Card::new(39),
             Card {
@@ -150,7 +200,7 @@ mod tests {
                 value: 40,
             }
         );
-        
+
         assert_eq!(
             Card::new(52),
             Card {
